6d4d3172fd2a0e8e9bb99c1b8757cca9cf90541b
Caleb CGates <calebcgates@gmail.com>
2021-05-13 18:02:31 -0700

    Pass onClick onShowCart through 2 levels of components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHanlder = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHanlder}/>
       <main>
         <Meals />
       </main>
